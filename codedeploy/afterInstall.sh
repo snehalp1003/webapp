04fb7824e01ba8a53ff7a6251aa1c6a9a88d139d
snehalp1003 <patel.sneha@northeastern.edu>
2020-07-04 18:10:39 -0400

    Making changes for AWS Config

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -1,6 +1,9 @@
 echo "Running after install script"
 cd /home/ubuntu/webapp
 sudo chown -R ubuntu:ubuntu /home/ubuntu/*
+sudo rm -rf /home/ubuntu/webapp/*.jpg
+sudo rm -rf /home/ubuntu/webapp/*.jpeg
+sudo rm -rf /home/ubuntu/webapp/*.png
 sudo chmod +x cloudwebapp-0.0.1-SNAPSHOT.jar
 
 #Kill application if already running
