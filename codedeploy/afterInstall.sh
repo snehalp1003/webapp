37e4771f750411619c84112cbe24789ec5367125
snehalp1003 <patel.sneha@northeastern.edu>
2020-07-04 22:07:47 -0400

    Committing code for cloudwatch-agent config along with statsd

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo "Running after install script"
 cd /home/ubuntu/webapp
 sudo chown -R ubuntu:ubuntu /home/ubuntu/*
@@ -9,3 +10,4 @@ kill -9 $(ps -ef|grep cloudwebapp-0.0.1 | grep -v grep | awk '{print $2}')
 source /etc/environment
 #Running application and appending logs
 nohup java -jar cloudwebapp-0.0.1-SNAPSHOT.jar > /home/ubuntu/webapplog.txt 2> /home/ubuntu/webapplog.txt < /home/ubuntu/webapplog.txt &
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/webapp/cloudwatch-agent.json -s
