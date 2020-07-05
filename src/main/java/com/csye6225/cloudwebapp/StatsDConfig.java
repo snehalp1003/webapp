0e6d7ce43ba1f6d2a5cac73967b2ab1abfd00717
snehalp1003 <patel.sneha@northeastern.edu>
2020-07-04 22:53:55 -0400

    Making changes to statsd config file

---
+++ src/main/java/com/csye6225/cloudwebapp/StatsDConfig.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.csye6225.cloudwebapp;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.timgroup.statsd.NoOpStatsDClient;
+import com.timgroup.statsd.NonBlockingStatsDClient;
+import com.timgroup.statsd.StatsDClient;
+
+/**
+ * @author Snehal Patel
+ *
+ */
+@Configuration
+public class StatsDConfig {
+
+    @Value("true")
+    private boolean publishMessage;
+
+    @Value("localhost")
+    private String metricHost;
+
+    @Value("8125")
+    private int portNumber;
+
+    @Value("csye6225")
+    private String prefix;
+
+    @Bean
+    public StatsDClient metricClient() {
+        if (publishMessage)
+            return new NonBlockingStatsDClient(prefix, metricHost, portNumber);
+        return new NoOpStatsDClient();
+    }
+}
