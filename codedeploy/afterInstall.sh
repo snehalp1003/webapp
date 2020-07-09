c14b3df19b00c78a3a41d2d97a596db2219ec8e9
snehalp1003 <patel.sneha@northeastern.edu>
2020-07-08 21:26:34 -0400

    Changes for auto reboot

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -5,17 +5,18 @@ cd /home/ubuntu/webapp
 sudo chown -R ubuntu:ubuntu /home/ubuntu/*
 sudo chmod +x cloudwebapp-0.0.1-SNAPSHOT.jar
 
-sudo cp /home/ubuntu/webapp/rebootWebapp.sh /etc/init.d
-
 #Kill application if already running
 kill -9 $(ps -ef|grep cloudwebapp-0.0.1 | grep -v grep | awk '{print $2}')
 
-source /etc/environment
+sudo cp webapp.service /etc/systemd/system
+sudo systemctl daemon-reload
+sudo systemctl stop webapp.service
+sudo systemctl daemon-reload
+sudo systemctl enable webapp.service
+sudo systemctl start webapp.service
+
+#source /etc/environment
 #Running application and appending logs
-nohup java -jar cloudwebapp-0.0.1-SNAPSHOT.jar > /home/ubuntu/webapplog.txt 2> /home/ubuntu/webapplog.txt < /home/ubuntu/webapplog.txt &
+#nohup java -jar cloudwebapp-0.0.1-SNAPSHOT.jar > /home/ubuntu/webapplog.txt 2> /home/ubuntu/webapplog.txt < /home/ubuntu/webapplog.txt &
 
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/webapp/cloudwatch-agent.json -s
-
-cd /etc/init.d
-sudo chmod +x rebootWebapp.sh
-sudo update-rc.d rebootWebapp.sh defaults
