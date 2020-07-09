12c29eb667afe6669ac8d4c878af2d14ec3b9ef5
snehalp1003 <patel.sneha@northeastern.edu>
2020-07-08 20:43:05 -0400

    Changes for auto reboot

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -5,9 +5,7 @@ cd /home/ubuntu/webapp
 sudo chown -R ubuntu:ubuntu /home/ubuntu/*
 sudo chmod +x cloudwebapp-0.0.1-SNAPSHOT.jar
 
-sudo chmod +x rebootWebapp.sh
 sudo cp /home/ubuntu/webapp/rebootWebapp.sh /etc/init.d
-sudo update-rc.d rebootWebapp.sh defaults
 
 #Kill application if already running
 kill -9 $(ps -ef|grep cloudwebapp-0.0.1 | grep -v grep | awk '{print $2}')
@@ -17,3 +15,7 @@ source /etc/environment
 nohup java -jar cloudwebapp-0.0.1-SNAPSHOT.jar > /home/ubuntu/webapplog.txt 2> /home/ubuntu/webapplog.txt < /home/ubuntu/webapplog.txt &
 
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/webapp/cloudwatch-agent.json -s
+
+cd /etc/init.d
+sudo chmod +x rebootWebapp.sh
+sudo update-rc.d rebootWebapp.sh defaults
